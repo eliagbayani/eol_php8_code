b5d385cc7ee9548cff758d42e3723bd05a4db1c8
eliagbayani <eagbayani@eol.org>
2025-08-06 13:05:34 +0800

    Update buildup_predicates.php

--- update_resources/connectors/buildup_predicates.php
+++ update_resources/connectors/buildup_predicates.php
@@ -1,6 +1,6 @@
 <?php
 namespace php_active_record;
-/* */
+/* Main starting point for generating CSV files for loading into Neo4j */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 // /* during development
 ini_set('error_reporting', E_ALL);
@@ -9,7 +9,6 @@ $GLOBALS['ENV_DEBUG'] = true; //set to true during development
 // */
 ini_set('memory_limit','8096M');
 $timestart = time_elapsed();
-
 // print_r($argv);
 $params['jenkins_or_cron'] = @$argv[1]; //not needed here
 $param                     = json_decode(@$argv[2], true); // print_r($param); exit;
@@ -17,6 +16,7 @@ $resource_id = $param['resource_id'];
 
 require_library('connectors/GenerateCSV_4Neo4j');
 $func = new GenerateCSV_4Neo4j();
-$func->buildup_predicates();
+// $func->buildup_predicates(); //Done
+$func->assemble_taxa();
 
 ?>

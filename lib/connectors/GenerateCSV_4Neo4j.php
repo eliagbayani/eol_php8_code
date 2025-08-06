9b667e51bdeeea4335044fb102d07cacd7a38206
eliagbayani <eagbayani@eol.org>
2025-08-07 02:57:32 +0800

    Update GenerateCSV_4Neo4j.php

--- lib/connectors/GenerateCSV_4Neo4j.php
+++ lib/connectors/GenerateCSV_4Neo4j.php
@@ -54,7 +54,7 @@ class GenerateCSV_4Neo4j
     {
         echo "\nprocess_table: [$what] [$meta->file_uri]...\n"; $i = 0;
         foreach(new FileIterator($meta->file_uri) as $line => $row) { $i++;
-            if(($i % 100000) == 0) echo "\n".number_format($i)." - ";
+            if(($i % 500000) == 0) echo "\n".number_format($i)." - ";
             if($meta->ignore_header_lines && $i == 1) continue;
             if(!$row) continue;
             // $row = Functions::conv_to_utf8($row); //possibly to fix special chars. but from copied template
@@ -138,13 +138,18 @@ class GenerateCSV_4Neo4j
 
         // fwrite($WRITE, ":START_ID(Taxon),associationType,referenceID,:END_ID(Taxon),:TYPE"."\n");
         $name1 = ''; $name2 = '';
-        if($ret = @$this->taxon[$rec['occurrenceID']]) $name1 = $ret['cN'];
-        else {
-            $print_r($rec); exit("\n1st oID not found\n");
+        
+        if($taxonID = $this->occurrence[$rec['occurrenceID']]) {
+            if($ret = @$this->taxon[$taxonID]) $name1 = $ret['cN'];
+            else {
+                print_r($rec); exit("\n1st oID not found\n");
+            }
         }
-        if($ret = @$this->taxon[$rec['targetOccurrenceID']]) $name2 = $ret['cN'];
-        else {
-            print_r($rec); exit("\n2nd oID not found\n");
+        if($taxonID = $this->occurrence[$rec['targetOccurrenceID']]) {
+            if($ret = @$this->taxon[$taxonID]) $name2 = $ret['cN'];
+            else {
+                print_r($rec); exit("\n2nd oID not found\n");
+            }
         }
 
         if($name1 && $name2) {

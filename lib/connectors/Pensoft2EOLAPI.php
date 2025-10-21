5bd575367a7c2cc3d4dd3f30930fb2f06e1f4418
eliagbayani <eagbayani@eol.org>
2025-10-21 23:38:49 +0800

    Update Pensoft2EOLAPI.php

--- lib/connectors/Pensoft2EOLAPI.php
+++ lib/connectors/Pensoft2EOLAPI.php
@@ -526,15 +526,23 @@ class Pensoft2EOLAPI extends Functions_Pensoft
                 
                 // exit("\n[".$this->param['resource_id']."]\nelix\n"); //good debug
                 /* === Edit for new resources === */
-                /* assign ontologies assign ontology */
-                if(in_array($this->param['resource_id'], array("10088_5097_ENV"))) $this->ontologies = "envo,eol-geonames";
-                elseif(in_array($this->param['resource_id'], array("10088_6943_ENV"))) $this->ontologies = "envo,eol-geonames,growth";
-                elseif(stripos($this->param['resource_id'], "SCtZ-") !== false)        $this->ontologies = "envo,eol-geonames"; //string is found
-                elseif(stripos($this->param['resource_id'], "scb-") !== false)         $this->ontologies = "envo,eol-geonames,growth"; //string is found
-                elseif(stripos($this->param['resource_id'], "scz-") !== false)         $this->ontologies = "envo,eol-geonames"; //string is found
+                /* assign ontologies assign ontology
+                growth: Smithsonian Contributions to Botany
+                        BHL_plants
+                e.g. https://api.pensoft.net/annotator?text=I%20like%20playing%20in%20the%20shrub&ontologies=growth
+
+                WoRMS with title == 'distribution' => eol-geonames
+                Wikipedia inferred => envo
+                TreatmentBank => envo
+                */
+                if(in_array($this->param['resource_id'], array("10088_5097_ENV"))) $this->ontologies = "envo,eol-geonames";             //Smithsonian Contributions to Zoology
+                elseif(in_array($this->param['resource_id'], array("10088_6943_ENV"))) $this->ontologies = "envo,eol-geonames,growth";  //Smithsonian Contributions to Botany
+                elseif(stripos($this->param['resource_id'], "SCtZ-") !== false)        $this->ontologies = "envo,eol-geonames"; //string is found           //SI Contributions to Zoology
+                elseif(stripos($this->param['resource_id'], "scb-") !== false)         $this->ontologies = "envo,eol-geonames,growth"; //string is found    //SI Contributions to Botany
+                elseif(stripos($this->param['resource_id'], "scz-") !== false)         $this->ontologies = "envo,eol-geonames"; //string is found           //SI Contributions to Zoology
                 elseif(in_array($this->param['resource_id'], array("118935_ENV")))     $this->ontologies = "eol-geonames";
                 if(in_array($this->param['resource_id'], array("120081_ENV", "120082_ENV", "118986_ENV", "118920_ENV", "120083_ENV", 
-                    "118237_ENV", "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV",
+                    "118237_ENV", "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV", //MoftheAES_ENV: Memoirs of the American Entomological Society
                     "120602_ENV", "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
                     "15423_ENV", "91155_ENV"))) $this->ontologies = "envo,eol-geonames";
                 elseif($this->param['resource'] == 'all_BHL') $this->ontologies = "envo,eol-geonames";

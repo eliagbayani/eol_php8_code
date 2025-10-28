5a52b8e1eb59a2d2e8f3c31b359eb1918c622b77
eliagbayani <eagbayani@eol.org>
2025-10-28 12:54:38 +0800

    Update TraitAnnotatorAPI.php

--- lib/connectors/TraitAnnotatorAPI.php
+++ lib/connectors/TraitAnnotatorAPI.php
@@ -8,19 +8,48 @@ These ff. workspaces work together:
 class TraitAnnotatorAPI
 {
     var $initializedYN = false;
+
+    var $uri_in_question = array();
+    var $uri_in_question_current = array();
+    var $uri_katjauri_map = array();
+    var $uris_with_new_kwords = array();
+
+
     function __construct()
     {
     }
     function initialize()
     {
-        echo "\nInitialize...\n";
+        echo "\nInitializing...\n";
         $this->initializedYN = true;
+        require_library('connectors/TextmineKeywordMapAPI');
+        $func = new TextmineKeywordMapAPI();
+        $func->get_keyword_mappings();
+        $this->uris_with_new_kwords     = $func->uris_with_new_kwords; //n=15
+        $this->uri_in_question          = $func->uri_in_question; //print_r($func->uri_in_question); exit;
+        $this->uri_in_question_current  = $func->uri_in_question_current;        
+        $this->uri_katjauri_map         = $func->uri_katjauri_map;        
+
+        unset($func);
+        unset($func->uris_with_new_kwords);
+        unset($func->uri_in_question);
+        unset($func->uri_in_question_current);
+        unset($func->uri_katjauri_map);
+
+        echo "\nuri_in_question 2: ".count($this->uri_in_question);
+        echo "\nuri_in_question_current 2: ".count($this->uri_in_question_current);
+        echo "\nuri_katjauri_map 2: ".count($this->uri_katjauri_map);
+        echo "\nuris_with_new_kwords: ".count($this->uris_with_new_kwords); //print_r($this->uris_with_new_kwords);
+
+        print_r($this->uri_in_question);
+        print_r($this->uris_with_new_kwords);
+
     }
     function annotate($params)
     {
-        print_r($params);
-        echo "\nStart here...\n";
         if($this->initializedYN) echo "\nInitialized OK\n";
         else echo "\nNot yet initialized.\n";
+        print_r($params);
+        echo "\nStart here...\n";
     }
 }

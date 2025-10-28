532aa69879e0810ed0b25d25dcde1acf5df081d8
eliagbayani <eagbayani@eol.org>
2025-10-28 13:34:11 +0800

    annotator WIP

---
+++ lib/connectors/TextmineKeywordMapAnnotate.php
@@ -0,0 +1,157 @@
+<?php
+namespace php_active_record;
+/* copied from TextmineKeywordMapAPI.php
+
+task: trait annotation
+Worksheets related:
+- TraitAnnotator
+*/
+class TextmineKeywordMapAnnotate
+{
+    public $params; // Declare the property
+    public $func;
+    // public $uri_in_question, $uri_in_question_current, $uri_katjauri_map;
+    // public $new_keywords_string_uri, $uris_with_new_kwords;
+    public $keyword_uri;
+    function __construct()
+    {   /* Please remove all keywords that currently map to these uris: */
+
+        /* copied template
+        $temp = array("http://purl.obolibrary.org/obo/ENVO_00000081", "http://purl.obolibrary.org/obo/ENVO_01000342", "http://purl.obolibrary.org/obo/ENVO_01000340", "http://purl.obolibrary.org/obo/ENVO_00000080", "http://purl.obolibrary.org/obo/ENVO_00000381", "http://purl.obolibrary.org/obo/ENVO_01000333", "http://purl.obolibrary.org/obo/ENVO_00000497", "http://purl.obolibrary.org/obo/ENVO_00000287", "http://purl.obolibrary.org/obo/ENVO_01000253", "http://purl.obolibrary.org/obo/ENVO_01000252", "http://purl.obolibrary.org/obo/ENVO_01000687", "http://purl.obolibrary.org/obo/ENVO_00000100", "http://purl.obolibrary.org/obo/ENVO_01000143", "http://purl.obolibrary.org/obo/ENVO_00000091", "http://purl.obolibrary.org/obo/ENVO_00000475");
+        $this->uris_with_new_kwords = array_flip($temp);
+        */
+
+        /* Then add the strings that are mapped to terms on the following worksheets:
+        - strings related to river/stream [river] n=422
+            https://docs.google.com/spreadsheets/d/1G3vCRvoJsYijqvJXwOooKdGj-jEiBop2EOl3lUxEny0/edit?gid=653002583#gid=653002583
+        - strings related to lake [lake] n=19
+            https://docs.google.com/spreadsheets/d/1G3vCRvoJsYijqvJXwOooKdGj-jEiBop2EOl3lUxEny0/edit?gid=974642361#gid=974642361
+        - strings related to mountain [mountain etc.] n=328
+            https://docs.google.com/spreadsheets/d/1G3vCRvoJsYijqvJXwOooKdGj-jEiBop2EOl3lUxEny0/edit?gid=1677871921#gid=1677871921
+        - strings related to coastal [coastal] n=89
+            https://docs.google.com/spreadsheets/d/1G3vCRvoJsYijqvJXwOooKdGj-jEiBop2EOl3lUxEny0/edit?gid=1687183186#gid=1687183186 */
+        $params['spreadsheetID'] = '1G3vCRvoJsYijqvJXwOooKdGj-jEiBop2EOl3lUxEny0';
+        $params['expire_seconds'] = 60*60*24*1; //1 day cache is ideal OK
+
+        $params['range'] = 'river!A1:D430'; //where "A" is the starting column, "D" is the ending column, and "1" is the starting row.
+        $this->params['river'] = $params;
+
+        $params['range'] = 'lake!A1:D30';
+        $this->params['lake'] = $params;
+
+        $params['range'] = 'mountain etc.!A1:D340';
+        $this->params['mountain'] = $params;
+
+        $params['range'] = 'coastal!A1:D100';
+        $this->params['coastal'] = $params;
+
+        require_library('connectors/GoogleClientAPI');
+        $this->func = new GoogleClientAPI();
+
+        /* copied template
+        $this->uri_in_question_current = array();   //temporary until spreadsheet adds 4th column.
+        $this->uri_katjauri_map = array();
+        */
+    }
+    function get_keyword_mappings($item = false)
+    {
+        if(!$item) $items = array('river', 'lake', 'mountain', 'coastal');
+        else       $items = array($item);
+        foreach($items as $what) { echo "\nAccessing [$what]...\n";
+            $params = $this->params[$what]; //$what e.g. 'coastal'
+            $arr = $this->func->access_google_sheet($params);
+            self::massage_result($arr, $what);
+        }
+        /* copied template
+        echo "\nuri_in_question 1: ".count($this->uri_in_question);
+        echo "\nuri_in_question_current 1: ".count($this->uri_in_question_current);
+        */
+    }
+    private function massage_result($arr, $what)
+    {
+        //start massage array
+        $i = 0;
+        foreach($arr as $item) { $i++;
+            if($i == 1) $fields = $item;
+            else {
+                $rek = array(); $k = 0;
+                foreach($fields as $fld) {
+                    if($fld) $rek[$fld] = @$item[$k];
+                    $k++;
+                }
+                @$ctr++;
+                $rek['new_uid'] = "NEW_".$ctr;
+                // $rek = array_map('trim', $rek); //works ok but needs all sheet columns filled up. Replaced by array_map_eol() below.
+                $rek = Functions::array_map_eol($rek); // print_r($rek);
+                if($what == 'coastal') $rek['uri'] = "http://purl.obolibrary.org/obo/ENVO_01000687";
+                /*Array(
+                    [match string] => at forest stream
+                    [value] => riparian zone
+                    [uri] => https://www.wikidata.org/entity/Q13360049
+                    [current_uri] => http....
+                    [notes] => Other resources
+                    [new_uid] => NEW_1
+                )*/
+                $uri = @$rek['uri'];
+                $current_uri = @$rek['current_uri'];
+                $match_string = @$rek['match string'];
+                if($uri && $match_string) {
+                    $this->keyword_uri[$match_string][] = $uri;
+                    $this->keyword_uri[$match_string] = array_unique($this->keyword_uri[$match_string]); //make values unique
+                }
+                /* copied template
+                if($current_uri && $match_string) {
+                    $this->uri_in_question_current[$current_uri][] = $match_string;
+                    $this->uri_in_question_current[$current_uri] = array_unique($this->uri_in_question_current[$current_uri]); //make values unique
+                    $this->uri_katjauri_map[$current_uri] = $uri; 
+                }
+                */
+            }
+        }
+        // end massage array
+    }
+    function xxx($params) 
+    {   
+        require_library('connectors/GoogleClientAPI');
+        $func = new GoogleClientAPI(); //get_declared_classes(); will give you how to access all available classes
+        $params = $this->params['coastal'];
+        $arr = $func->access_google_sheet($params); 
+        /* IMPORTANT:
+        if 1st param has $params['expire_seconds'], will follow value accordingly
+        else
+            2nd param if blank, will use cache
+            2nd param if true, will use cache
+            2nd param if false, will re-access remote
+        */
+
+        //start massage array
+        $i = 0;
+        foreach($arr as $item) { $i++;
+            if($i == 1) $fields = $item;
+            else {
+                $rek = array(); $k = 0;
+                foreach($fields as $fld) {
+                    if($fld) $rek[$fld] = @$item[$k];
+                    $k++;
+                }
+                @$ctr++;
+                $rek['new_uid'] = "NEW_".$ctr;
+                $rek = array_map('trim', $rek);
+                print_r($rek); exit;
+            }
+        }
+        // end massage array
+
+
+        /* copied template
+        $source = "/Volumes/AKiTiO4/d_w_h/last_smasher/test/taxonomy.tsv"; //results_2021_02_24.zip
+        $destination = "/Volumes/AKiTiO4/d_w_h/last_smasher/test/final_taxonomy.tsv";
+        $WRITE = Functions::file_open($destination, "w");
+        $i = 0;
+        foreach(new FileIterator($source) as $line => $row) { $i++;
+            $rek = array_map('trim', $rek);
+        }
+        */
+    }
+}
+?>
--- lib/connectors/TraitAnnotatorAPI.php
+++ lib/connectors/TraitAnnotatorAPI.php
@@ -9,11 +9,11 @@ class TraitAnnotatorAPI
 {
     var $initializedYN = false;
 
-    var $uri_in_question = array();
-    var $uri_in_question_current = array();
-    var $uri_katjauri_map = array();
-    var $uris_with_new_kwords = array();
-
+    // var $uri_in_question = array();
+    // var $uri_in_question_current = array();
+    // var $uri_katjauri_map = array();
+    // var $uris_with_new_kwords = array();
+    public $keyword_uri;
 
     function __construct()
     {
@@ -22,28 +22,13 @@ class TraitAnnotatorAPI
     {
         echo "\nInitializing...\n";
         $this->initializedYN = true;
-        require_library('connectors/TextmineKeywordMapAPI');
-        $func = new TextmineKeywordMapAPI();
+        require_library('connectors/TextmineKeywordMapAnnotate');
+        $func = new TextmineKeywordMapAnnotate();
         $func->get_keyword_mappings();
-        $this->uris_with_new_kwords     = $func->uris_with_new_kwords; //n=15
-        $this->uri_in_question          = $func->uri_in_question; //print_r($func->uri_in_question); exit;
-        $this->uri_in_question_current  = $func->uri_in_question_current;        
-        $this->uri_katjauri_map         = $func->uri_katjauri_map;        
-
+        $this->keyword_uri     = $func->keyword_uri;
         unset($func);
-        unset($func->uris_with_new_kwords);
-        unset($func->uri_in_question);
-        unset($func->uri_in_question_current);
-        unset($func->uri_katjauri_map);
-
-        echo "\nuri_in_question 2: ".count($this->uri_in_question);
-        echo "\nuri_in_question_current 2: ".count($this->uri_in_question_current);
-        echo "\nuri_katjauri_map 2: ".count($this->uri_katjauri_map);
-        echo "\nuris_with_new_kwords: ".count($this->uris_with_new_kwords); //print_r($this->uris_with_new_kwords);
-
-        print_r($this->uri_in_question);
-        print_r($this->uris_with_new_kwords);
-
+        echo "\nkeyword_uri 2: ".count($this->keyword_uri);
+        print_r($this->keyword_uri);
     }
     function annotate($params)
     {

0aa404d14019ea09240866cc565a5135ebc8aef6
eliagbayani <eagbayani@eol.org>
2025-11-23 13:31:14 +0800

    Update annotator.php

--- update_resources/connectors/annotator.php
+++ update_resources/connectors/annotator.php
@@ -29,7 +29,7 @@ include_once(dirname(__FILE__) . "/../../config/environment.php");
 // /* during development
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
-$GLOBALS['ENV_DEBUG'] = true; //true; //set to true during development
+$GLOBALS['ENV_DEBUG'] = false; //true; //set to true during development
 // */
 // $GLOBALS['ENV_DEBUG'] = false;
 $timestart = time_elapsed();
@@ -54,6 +54,5 @@ if(!@$params['predicates']) { echo "\n+++++start3"; print_r($argv); exit("ERROR:
 
 require_library('connectors/TraitAnnotatorAPI');
 $func = new TraitAnnotatorAPI();
-// $params['predicates'] = "ALL"; //added but didn't work atm. So it is now commented.
 $func->annotate($params);
 ?>

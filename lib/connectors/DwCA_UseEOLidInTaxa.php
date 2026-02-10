b20c487887043ff05ac960f6b8d00ca7095a4e5d
eliagbayani <eagbayani@eol.org>
2026-02-10 17:46:48 +0800

    Update DwCA_UseEOLidInTaxa.php

--- lib/connectors/DwCA_UseEOLidInTaxa.php
+++ lib/connectors/DwCA_UseEOLidInTaxa.php
@@ -1,6 +1,6 @@
 <?php
 namespace php_active_record;
-/* connector: [called from DwCA_Utility.php, which is called from match_taxa_2DH.php] 
+/* connector: [called from DwCA_Utility.php, which is called from use_EOLid_as_taxonID.php] 
 These ff. workspaces work together:
 - generate_higherClassification_8.code-workspace
 - DHConnLib_8.code-workspace
@@ -8,6 +8,14 @@ These ff. workspaces work together:
 - DwCA_MatchTaxa2DH.code-workspace
 - UseEOLidInTaxon.code-workspace
 - GenerateCSV_4Neo4j.code-workspace
+
+If EOLid exists.
+This lib. will now use EOLid for: taxon->taxonID
+                                  vernacular->taxonID  
+                                  media->taxonID
+                                  occurrence->taxonID
+It will delete/exclude those taxon, vernaculars, media, occurrence and MoF without the EOLid.
+*It can also assign DH taxonRank and canonicalName to resource document, but not anymore.
 */
 use \AllowDynamicProperties; //for PHP 8.2
 #[AllowDynamicProperties] //for PHP 8.2
@@ -33,16 +41,25 @@ class DwCA_UseEOLidInTaxa
         $extensions = array_keys($tables); print_r($extensions);
         // */
 
-        $meta = $tables['http://rs.tdwg.org/dwc/terms/taxon'][0];
-        self::process_table($meta, 'build_taxon_info');
-        self::process_table($meta, 'write_taxon');
+        if($meta = @$tables['http://rs.tdwg.org/dwc/terms/taxon'][0]) {
+            self::process_table($meta, 'build_taxon_info');
+            /* working but may not be needed anymore
+            self::get_DH_info_4EOLids();
+            */
+            self::process_table($meta, 'write_taxon');        
+        }
+        else exit("\nERROR: Cannot proceed without Taxon extension.\n");
 
-        $meta = $tables['http://rs.tdwg.org/dwc/terms/occurrence'][0];
-        self::process_table($meta, 'write_occurrence');
+        if($meta = @$tables['http://rs.tdwg.org/dwc/terms/occurrence'][0]) {
+            self::process_table($meta, 'write_occurrence');
+        }
+        else exit("\nERROR: Cannot proceed without Occurrence extension.\n");
 
-        if(in_array('http://rs.gbif.org/terms/1.0/vernacularname', $extensions)) {
-            $meta = $tables['http://rs.gbif.org/terms/1.0/vernacularname'][0];
-            self::process_table($meta, 'write_vernacular');
+        if($meta = @$tables['http://rs.gbif.org/terms/1.0/vernacularname'][0]) {
+            self::process_table($meta, 'write_other_extensions', 'vernacular');
+        }
+        if($meta = $tables['http://eol.org/schema/media/document'][0]) {
+            self::process_table($meta, 'write_other_extensions', 'document');
         }
 
         /* not needed since no taxonID here
@@ -53,7 +70,16 @@ class DwCA_UseEOLidInTaxa
 
         if($this->debug) Functions::start_print_debug($this->debug, $this->resource_id); //works OK
     }
-    private function process_table($meta, $what)
+    private function get_DH_info_4EOLids()
+    {
+        require_library('connectors/DHConnLib');
+        $func = new DHConnLib(1);
+        $this->DH_subset_info = $func->grab_from_DH('get_DH_info_forEOLids', $this->EOLids);
+        unset($func);
+        // print_r($this->DH_subset_info); exit("\nstop muna 1\n");
+        echo "\nDH_subset_info: ".count($this->DH_subset_info)."\n";
+    }
+    private function process_table($meta, $what, $class = false) //3rd param $class is optional
     { 
         echo "\nprocess_table: [$what] [$meta->file_uri]...\n";
         $i = 0;
@@ -99,40 +125,95 @@ class DwCA_UseEOLidInTaxa
 
             $taxonID = $rec['http://rs.tdwg.org/dwc/terms/taxonID'];
 
+            must re-think here. maybe we don't change the taxonID, taxonRank and scientificName. Only EOLid will be from DH
+
             if ($what == 'build_taxon_info') {
                 $EOLid = $rec['http://eol.org/schema/EOLid'];
                 if($EOLid) $this->taxonID_EOLid[$taxonID] = $EOLid;
-                else       $this->taxonID_EOLid[$taxonID] = $taxonID;
+                else       $this->taxonID_EOLid[$taxonID] = false;
+                // /* new step: Assign DH taxonRank and canonicalName to resource document.
+                if($EOLid) $this->EOLids[$EOLid] = '';
+                // */
             }
             if($what == 'write_taxon') {
                 if($new_taxonID = $this->taxonID_EOLid[$taxonID]) {
-                    if(isset($this->unique_taxonID[$new_taxonID])) continue; //prevented duplicate taxonIDs, which is possible e.g. Globi DwCA.
+                    if(isset($this->unique_taxonID[$new_taxonID])) {
+                        $this->debug['Duplicate taxonIDs']["$taxonID - $new_taxonID"] = '';
+                        continue; //prevented duplicate taxonIDs, which is possible e.g. Globi DwCA.
+                    }
                     else {
                         $this->unique_taxonID[$new_taxonID] = '';
                         $rec['http://rs.tdwg.org/dwc/terms/taxonID'] = $new_taxonID; //is the EOLid
 
+                        /* working but may not be used anymore
                         $rec['http://rs.tdwg.org/dwc/terms/vernacularName'] = self::get_vernacularName($new_taxonID);
+                        */
+
+                        /* working but may not be used anymore
+                        $rec = self::assign_DH_rank_and_canonical_to_resource_taxon($rec, $new_taxonID); //new assign DH taxonRank and canonicalName to resource taxon
+                        */
                     }
                 }
 
                 if($parentNameUsageID = @$rec['http://rs.tdwg.org/dwc/terms/parentNameUsageID']) {
                     if($new_taxonID = @$this->taxonID_EOLid[$parentNameUsageID]) $rec['http://rs.tdwg.org/dwc/terms/parentNameUsageID'] = $new_taxonID;
+                    else $rec['http://rs.tdwg.org/dwc/terms/parentNameUsageID'] = ''; //since there is no EOLid for this parentID, we set it to blank.
                 }
 
                 self::write_2archive($rec, 'taxon'); continue;                
             }
             if ($what == 'write_occurrence') {
                 if($new_taxonID = @$this->taxonID_EOLid[$taxonID]) $rec['http://rs.tdwg.org/dwc/terms/taxonID'] = $new_taxonID;
-                else @$this->debug['Taxa in Occur.tab but not in Taxon.tab'] .= " [$taxonID]";
+                else {
+                    @$this->debug['Taxa in Occur.tab but not in Taxon.tab'][$taxonID] = '';
+                    $this->occurrenceID_to_delete[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
+                    continue; //don't save
+                }
                 self::write_2archive($rec, 'occurrence_specific'); continue;
             }
-            if ($what == 'write_vernacular') {
-                if($new_taxonID = @$this->taxonID_EOLid[$taxonID]) $rec['http://rs.tdwg.org/dwc/terms/taxonID'] = $new_taxonID;
-                else @$this->debug['Taxa in Vernacular.tab but not in Taxon.tab'] .= " [$taxonID]";
-                self::write_2archive($rec, 'vernacular'); continue;
+            if ($what == 'write_other_extensions') {
+                if($class == 'document') {
+                    if($rec['http://purl.org/dc/terms/type'] == 'http://purl.org/dc/dcmitype/Text') {
+                        if($new_taxonID = @$this->taxonID_EOLid[$taxonID]) $rec['http://rs.tdwg.org/dwc/terms/taxonID'] = $new_taxonID;
+                        else {
+                            @$this->debug["Taxa in $class .tab but not in Taxon.tab"][$taxonID] = '';                            
+                            continue; //don't save
+                        }
+                    }
+                }
+                elseif($class == 'vernacular') {
+                    if($new_taxonID = @$this->taxonID_EOLid[$taxonID]) $rec['http://rs.tdwg.org/dwc/terms/taxonID'] = $new_taxonID;
+                    else {
+                        @$this->debug["Taxa in $class .tab but not in Taxon.tab"][$taxonID] = '';
+                        continue; //don't save
+                    }
+                }
+                self::write_2archive($rec, $class); continue;
             }
 
             // if($i >= 100) break; //dev only
+        }
+    }
+    private function assign_DH_rank_and_canonical_to_resource_taxon($rec, $new_taxonID)
+    {
+        $taxonRank = $rec['http://rs.tdwg.org/dwc/terms/taxonRank'];
+        $canonicalName = $rec['http://rs.gbif.org/terms/1.0/canonicalName'];
+        if($ret = @$this->DH_subset_info[$new_taxonID]) { //print_r($ret); exit;
+            /*Array(
+                [r] => kingdom
+                [c] => Metazoa
+            )*/
+            if($taxonRank != $ret['r'])     $rec['http://rs.tdwg.org/dwc/terms/taxonRank']     = $ret['r']; //gets the value from DH
+            if($canonicalName != $ret['c']) $rec['http://rs.gbif.org/terms/1.0/canonicalName'] = $ret['c']; //gets the value from DH
+            // Below is just for stats. Let us capture it if needed.
+            if($taxonRank != $ret['r']) {
+                // print_r($rec); exit("\nPartner taxonRank is different from DH taxonRank (".$ret['r'].")\n");
+                $this->debug['Partner taxonRank is different from DH taxonRank']["($taxonRank)(".$ret['r'].")"] = '';
+            }
+            if($canonicalName != $ret['c']) {
+                // print_r($rec); exit("\nPartner canonicalName is different from DH canonicalName (".$ret['c'].")\n");
+                $this->debug['Partner canonicalName is different from DH canonicalName']["($canonicalName)(".$ret['c'].")"] = '';
+            }
         }
     }
     private function write_2archive($rec, $class)
@@ -151,10 +232,10 @@ class DwCA_UseEOLidInTaxa
         elseif($class == "occurrence")          $c = new \eol_schema\Occurrence();
         elseif($class == "occurrence_specific") $c = new \eol_schema\Occurrence_specific();
         elseif($class == "vernacular")          $c = new \eol_schema\VernacularName();
+        elseif($class == "document")             $c = new \eol_schema\MediaResource();
         /* not used here
         elseif($class == "agent")                $c = new \eol_schema\Agent();
         elseif($class == "reference")            $c = new \eol_schema\Reference();
-        elseif($class == "document")             $c = new \eol_schema\MediaResource();
         elseif($class == "measurementorfact")    $c = new \eol_schema\MeasurementOrFact();
         elseif($class == "association")          $c = new \eol_schema\Association();
         */

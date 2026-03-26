597b375ce054e1f9d186d1401942511cf8d7200e
eliagbayani <eagbayani@eol.org>
2026-03-27 02:32:05 +0800

    Update aggregate_neo4j_CSVs.php

--- update_resources/connectors/aggregate_neo4j_CSVs.php
+++ update_resources/connectors/aggregate_neo4j_CSVs.php
@@ -15,6 +15,27 @@ $timestart = time_elapsed();
 // $params['jenkins_or_cron'] = @$argv[1]; //not needed here
 // $param                     = json_decode(@$argv[2], true); //print_r($param); exit;
 // $resource_id = $param['resource_id'];
+
+
+/* Working OK
+$arr['combined_CSVs']['edges']['file_1.csv'] = 'destination 1';
+$arr['combined_CSVs']['edges']['file_2.csv'] = 'destination 2';
+$arr['combined_CSVs']['edges']['file_3.csv'] = 'destination 3';
+$arr['combined_CSVs']['nodes']['file_4.csv'] = 'destination 4';
+$arr['combined_CSVs']['nodes']['file_5.csv'] = 'destination 5';
+$arr['combined_CSVs']['nodes']['file_6.csv'] = 'destination 6';
+$a = $arr['combined_CSVs'];
+foreach($a as $path => $filenames) {
+    echo "\n-----\npath: $path\n";
+    print_r($filenames);
+    foreach($filenames as $fname => $destination) {
+        echo "\n[$fname] [$destination]";
+    }
+}
+exit("\n-end test-\n");
+*/
+
+
 $param = array();
 require_library('connectors/AggregateCSV_4Neo4j');
 $func = new AggregateCSV_4Neo4j($param);

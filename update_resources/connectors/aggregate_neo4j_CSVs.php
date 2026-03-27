7581870bcc20ce072cb34cff02572d8c6f7b36a1
eliagbayani <eagbayani@eol.org>
2026-03-27 14:31:50 +0800

    Update aggregate_neo4j_CSVs.php

--- update_resources/connectors/aggregate_neo4j_CSVs.php
+++ update_resources/connectors/aggregate_neo4j_CSVs.php
@@ -36,36 +36,59 @@ exit("\n-end test-\n");
 */
 
 // /* Working OK ?
-$arr['worms_CSVs']['nodes']['file_3.csv'] = 1;
-$arr['worms_CSVs']['nodes']['file_1.csv'] = 2;
-$arr['worms_CSVs']['nodes']['file_2.csv'] = 3;
-$arr['worms_CSVs']['edges']['FILE_6.csv'] = 4;
-$arr['worms_CSVs']['edges']['FILE_4.csv'] = 5;
-$arr['worms_CSVs']['edges']['FILE_5.csv'] = 6;
+$this_path_stats = CONTENT_RESOURCE_LOCAL_PATH . 'neo4j_stats';
+if(!is_dir($this_path_stats)) mkdir($this_path_stats);
 
-$arr['globi_CSVs']['nodes']['file_9.csv'] = 7;
-$arr['globi_CSVs']['nodes']['file_7.csv'] = 8;
-$arr['globi_CSVs']['nodes']['file_8.csv'] = 9;
-$arr['globi_CSVs']['edges']['FILE_12.csv'] = 10;
-$arr['globi_CSVs']['edges']['FILE_10.csv'] = 11;
-$arr['globi_CSVs']['edges']['FILE_11.csv'] = 12;
+$arr['worms_CSVs']['nodes']['file_3.csv'] = 10;
+$arr['worms_CSVs']['nodes']['file_1.csv'] = 20;
+$arr['worms_CSVs']['nodes']['file_2.csv'] = 30;
+$arr['worms_CSVs']['edges']['FILE_6.csv'] = 40;
+$arr['worms_CSVs']['edges']['FILE_4.csv'] = 50;
+$arr['worms_CSVs']['edges']['FILE_5.csv'] = 60;
+
+$arr['globi_CSVs']['nodes']['file_9.csv'] = 70;
+$arr['globi_CSVs']['nodes']['file_7.csv'] = 80;
+$arr['globi_CSVs']['nodes']['file_8.csv'] = 90;
+$arr['globi_CSVs']['edges']['FILE_12.csv'] = 100;
+$arr['globi_CSVs']['edges']['FILE_10.csv'] = 110;
+$arr['globi_CSVs']['edges']['FILE_11.csv'] = 120;
 
 $r = array_keys($arr);
 $r = array_unique($r); //make unique
 $r = array_values($r); //reindex key
 
-foreach($r as $resource_name) { echo "\n-----Resource: [$resource_name]\n";
+foreach($r as $resource_name) { echo "\n-----Resource: [$resource_name]";
+    $save_path = $this_path_stats.'/'.$resource_name.'.tsv';
     $a = $arr[$resource_name];
+    $values = array(); $headers = array();
+    $headers[] = 'Date';
+    $values[] = date('Y-m-d H:i:s A');
     foreach($a as $path => $filenames) {
         ksort($filenames); //important
-        echo "\n-----\npath: $path\n";
+        echo "\npath: $path";
         foreach($filenames as $fname => $total) {
             echo "\n[$fname] [total = $total]";
+            $headers[] = $fname;
+            $values[] = $total;
         }
     }
+
+    // if(!file_exists($save_path)) {
+        $WRITE = Functions::file_open($save_path, 'a');
+        array_unshift($filenames, "Date"); //add an element on the start of an array
+        fwrite($WRITE, implode("\t", $headers)."\n");
+    // }
+    $WRITE = Functions::file_open($save_path, 'a');
+    fwrite($WRITE, implode("\t", $values)."\n");
+    fclose($WRITE);        
+
 }
 
-// $save_path = $this->path['stats'].'/'.$resource_name.'.tsv';
+
+
+
+
+// $save_path = $this_path_stats.'/'.$resource_name.'.tsv';
 // $WRITE = Functions::file_open($save_path, 'a');
 // if(!file_exists($save_path)) {
 //     array_unshift($filenames, "Date"); //add an element on the start of an array

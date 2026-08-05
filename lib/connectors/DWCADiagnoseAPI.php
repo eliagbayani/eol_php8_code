1f94e7f5b61211f444f7e87a8a618d9e88450e4a
eliagbayani <eagbayani@eol.org>
2026-08-05 23:44:09 +0800

    Update DWCADiagnoseAPI.php

--- lib/connectors/DWCADiagnoseAPI.php
+++ lib/connectors/DWCADiagnoseAPI.php
@@ -381,10 +381,7 @@ class DWCADiagnoseAPI
 
         $undefined = array();
         foreach($parent_ids as $parent_id) {
-            if($parent_id == "Q109044861") {} //https://www.wikidata.org/wiki/Q109044861 - no label defined yet
-            else {
-                if(!isset($taxon_ids[$parent_id])) $undefined[$parent_id] = '';
-            }
+            if(!isset($taxon_ids[$parent_id])) $undefined[$parent_id] = '';            
         }
         if($write_2text_file) {
             foreach(array_keys($undefined) as $id) fwrite($WRITE, $id . "\n");

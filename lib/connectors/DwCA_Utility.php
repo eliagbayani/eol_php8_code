6e8291ac2fd09a8f771140d5bca878ddd888d7e1
eliagbayani <eagbayani@eol.org>
2026-08-23 18:17:57 +0800

    Update DwCA_Utility.php

--- lib/connectors/DwCA_Utility.php
+++ lib/connectors/DwCA_Utility.php
@@ -979,7 +979,9 @@ class DwCA_Utility
                 /* Need to have unique taxon ids. It is confined to a pre-defined list of resources bec. it is memory intensive and most resources have already unique taxon ids.
                 Useful for e.g. DATA-1724 resource 'plant_forms_habitat_and_distribution'.
                 */
-                if(in_array($this->resource_id, array("plant_forms_habitat_and_distribution-adjusted", "1000_final", "fwater_marine_image_bank_meta_recoded")) || in_array(substr($this->resource_id,0,3), array("LD_", "EOL"))) {
+                if(in_array($this->resource_id, array("plant_forms_habitat_and_distribution-adjusted", "1000_final", "fwater_marine_image_bank_meta_recoded", "PlantFormsHabiDistrib-with-hC")) 
+                    || in_array(substr($this->resource_id,0,3), array("LD_", "EOL"))) 
+                {
                     if($class == "taxon") {
                         if($field == "taxonID") {
                             $taxon_id = @$rec[$key];
@@ -1194,7 +1196,9 @@ class DwCA_Utility
 
                 // /* New: needed validations
                 if($class == "document") { //if object is text type then description cannot be blank.
-                    if(!$o->description && $o->type == 'http://purl.org/dc/dcmitype/Text') continue;
+                    if($o->type == 'http://purl.org/dc/dcmitype/Text') {
+                        if(!$o->description) continue;
+                    }
                     $this->debug[$this->resource_id.' Titles'][@$o->title] = '';
                 }
                 // */

ab801311459cd3fc67d99546cea67270ddbd8895
eliagbayani <eagbayani@eol.org>
2026-09-24 20:31:36 +0800

    Update match_taxa_2DH.php

--- update_resources/connectors/match_taxa_2DH.php
+++ update_resources/connectors/match_taxa_2DH.php
@@ -25,7 +25,7 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 $GLOBALS['ENV_DEBUG'] = true; //set to true during development
 // */
-ini_set('memory_limit','10096M'); //8096M orig 9096M | TreatmentBank needs 10096M
+ini_set('memory_limit','11096M'); //8096M orig | TreatmentBank needs 10096M | GBIF data coverage needs 11096M
 $timestart = time_elapsed();
 
 /* file() converts rows into an array
